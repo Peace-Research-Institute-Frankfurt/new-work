3731942193292be1c8bd16b1769d98d0cc32a0e6
Max Kohler <hi@maxkoehler.com>
2022-11-15 12:09:47 +0100

    List pages in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,26 @@
 import React from "react";
 import * as styles from "./SiteFooter.module.scss";
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql, Link, useStaticQuery } from "gatsby";
 
 export default function Footer() {
   const data = useStaticQuery(graphql`
     query {
       meta: site {
         buildTime(formatString: "D MMMM Y, HH:mm")
+      },
+      pages: allFile(filter: { extension: { eq: "mdx" }, sourceInstanceName: { eq: "pages" } }, sort: { fields: childMdx___frontmatter___order }) {
+        nodes {
+          id
+          childMdx {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              order
+            }
+          }
+        }
       }
     }
   `);
@@ -14,15 +28,9 @@ export default function Footer() {
     <footer className={styles.container}>
       <nav className={styles.nav}>
         <ul>
-          <li>
-            <a href="#1">Impressum</a>
-          </li>
-          <li>
-            <a href="#1">Datenschutz</a>
-          </li>
-          <li>
-            <a href="#1">Barrierefreiheit</a>
-          </li>
+          {data.pages.nodes.map(p => {
+            return (<li><Link to={p.childMdx.fields.slug}>{p.childMdx.frontmatter.title}</Link></li>)
+          })}
         </ul>
       </nav>
       <p>New Work (Eine Anleitung) ist eine Publikation der Hessischen Stiftung für Friedens- und Konfliktforschung (HSFK).</p>
