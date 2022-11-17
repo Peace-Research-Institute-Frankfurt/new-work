36afdf72575a12a594522b63feb34dc55335ec94
Max Kohler <hello@maxkohler.com>
2022-11-17 17:40:52 +0100

    Enable markdown in citations

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,10 +1,14 @@
 import React from "react";
 import * as styles from "./Quote.module.scss";
+import MarkdownRenderer from "react-markdown-renderer";
+
 export default function Quote(props) {
   return (
     <blockquote className={styles.container}>
       <div className={styles.text}>{props.children}</div>
-      <cite className={styles.cite}>{props.cite}</cite>
+      <cite className={styles.cite}>
+        <MarkdownRenderer markdown={props.cite} />
+      </cite>
     </blockquote>
   );
 }
