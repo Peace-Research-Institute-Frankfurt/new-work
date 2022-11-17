e98d5a0a5ea9bb76d1e0a9ea6632ada2d1dc5824
Max Kohler <hello@maxkohler.com>
2022-11-17 18:06:30 +0100

    Set square favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/leibniz-head.png",
+        icon: "src/images/favicon.png",
       },
     },
     {
