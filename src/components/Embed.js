d6185cbdf1629e835f1084df608d0417a0a2afd8
Max Kohler <hello@maxkohler.com>
2022-11-23 15:54:24 +0100

    Iterate embed component

--- src/components/Embed.js
+++ src/components/Embed.js
@@ -4,7 +4,7 @@ import MarkdownRenderer from "react-markdown-renderer";
 import * as styles from "./Embed.module.scss";
 import { EmbedChoicesContext } from "../context/EmbedChoicesContext";
 
-function Embed({ src, caption, title, provider }) {
+function Embed({ src, caption, title, provider, width, height }) {
   const data = useStaticQuery(graphql`
     query {
       providers: allEmbedProvidersJson {
@@ -15,27 +15,32 @@ function Embed({ src, caption, title, provider }) {
       }
     }
   `);
+  const { embedChoices, toggleEmbedChoice } = useContext(EmbedChoicesContext);
+  const [temporaryActive, setTemporaryActive] = useState(false);
+  let isActive = false;
+  if (embedChoices[provider] === true) {
+    isActive = true;
+  }
+
+  const baseId = useId();
+
   let providerData = null;
   data.providers.nodes.forEach((p) => {
     if (p.provider === provider) {
       providerData = p;
     }
   });
-  const [isActive, setIsActive] = useState(false);
-  const { embedChoices, toggleEmbedChoice } = useContext(EmbedChoicesContext);
-  const baseId = useId();
 
-  let isChecked = false;
-  if (embedChoices[provider] === true) {
-    isChecked = true;
-  }
+  const embedStyles = {
+    paddingTop: `${(height / width) * 100}%`,
+  };
 
   function handleLoadClick() {
-    setIsActive(true);
+    setTemporaryActive(true);
   }
-  const rememberInputId = `remember-embed-choice-${baseId}`;
+
   return (
-    <figure>
+    <figure className={styles.container}>
       {!isActive && (
         <div className={styles.consent}>
           <MarkdownRenderer markdown={providerData.description} />
@@ -43,12 +48,12 @@ function Embed({ src, caption, title, provider }) {
             <button className={styles.button} onClick={handleLoadClick}>
               Eingebetteten Inhalt laden
             </button>
-            <label htmlFor={rememberInputId}>
+            <label htmlFor={`embed-choice-${baseId}`}>
               Entscheidung speichern?
               <input
                 type="checkbox"
-                id={rememberInputId}
-                checked={isChecked}
+                id={`embed-choice-${baseId}`}
+                checked={isActive}
                 onChange={(e) => {
                   toggleEmbedChoice(provider);
                 }}
@@ -59,7 +64,7 @@ function Embed({ src, caption, title, provider }) {
       )}
       {isActive && (
         <>
-          <div className={styles.iframeContainer}>
+          <div className={styles.iframeContainer} style={embedStyles}>
             <iframe
               title={title}
               src={src}
@@ -68,20 +73,20 @@ function Embed({ src, caption, title, provider }) {
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             />
           </div>
-          <figcaption>{caption}</figcaption>
+          <figcaption className={styles.caption}>{caption}</figcaption>
         </>
       )}
     </figure>
   );
 }
 
-function Vimeo({ url, width, height }) {
+function Vimeo({ url, width, height, caption }) {
   const matches = url.match(/(?:player.vimeo.com\/video\/)(\d+)/);
   let src = null;
   if (matches && matches[1]) {
     src = `https://player.vimeo.com/video/${matches[1]}?h=0e92d36ba9&title=0&byline=0&portrait=0`;
   }
-  return <Embed provider="vimeo" width={width} height={height} src={src} />;
+  return <Embed provider="vimeo" width={width} height={height} src={src} caption={caption} />;
 }
 
 function Youtube({ url, title, caption, width, height }) {
@@ -92,7 +97,7 @@ function Youtube({ url, title, caption, width, height }) {
       height={height}
       title={title}
       src={`https://www.youtube-nocookie.com/embed/${matches[1]}`}
-      caption="This is a youtube embed"
+      caption={caption}
       provider="youtube"
     />
   );
