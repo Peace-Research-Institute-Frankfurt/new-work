a2cffc74969f9f93fe74672147bc3d16f688bdc7
Max Kohler <hello@maxkohler.com>
2022-11-26 18:45:22 +0100

    Remove unused prop

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,7 +2,7 @@ import React from "react";
 import * as styles from "./Quote.module.scss";
 import MarkdownRenderer from "react-markdown-renderer";
 
-export default function Quote({children, cite, image}) {
+export default function Quote({children, cite}) {
   return (
     <blockquote className={styles.container}>
       <div className={styles.text}>{children}</div>
