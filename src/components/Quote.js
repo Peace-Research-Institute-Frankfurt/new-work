c85049a539af35b923086f3246f3eac149adfe0e
Max Kohler <hello@maxkohler.com>
2022-11-26 18:45:03 +0100

    Destructure props

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,12 +2,12 @@ import React from "react";
 import * as styles from "./Quote.module.scss";
 import MarkdownRenderer from "react-markdown-renderer";
 
-export default function Quote(props) {
+export default function Quote({children, cite, image}) {
   return (
-    <blockquote className={`${styles.container} ${props.position && styles[props.position]}`}>
-      <div className={styles.text}>{props.children}</div>
+    <blockquote className={styles.container}>
+      <div className={styles.text}>{children}</div>
       <cite className={styles.cite}>
-        <MarkdownRenderer markdown={props.cite} />
+        <MarkdownRenderer markdown={cite} />
       </cite>
     </blockquote>
   );
