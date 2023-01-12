a31655ccf7eb3e92dfe29bf194dd371c16f7e571
Max Kohler <hello@maxkohler.com>
2023-01-12 14:46:32 +0100

    Iterate post header

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as styles from "./Logo.module.scss";
 import { graphql, Link, useStaticQuery } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function Logo() {
   const data = useStaticQuery(graphql`
@@ -14,9 +15,18 @@ export default function Logo() {
   `);
   return (
     <Link className={styles.container} to="/">
-      <div>
-        <span className={styles.top}>{data.site.siteMetadata.title}</span>
-      </div>
+      <Link className={styles.siteTitle} to="/">
+        <StaticImage
+          imgStyle={{ objectFit: "contain" }}
+          placeholder="none"
+          width={50}
+          layout="constrained"
+          className={styles.face}
+          src="../images/leibniz-head.png"
+          alt="New Work Logo"
+        />
+      </Link>
+      <span className={styles.label}>{data.site.siteMetadata.title}</span>
     </Link>
   );
 }
